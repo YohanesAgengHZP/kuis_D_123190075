3d7f1793baa2f54c2d31aad3671a2e694adcab0a
YohanesAgengHZP <81618432+YohanesAgengHZP@users.noreply.github.com>
2022-03-16 12:24:29 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "NoteAPP",
+      title: "Aplikasi Kaya Appstore",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
